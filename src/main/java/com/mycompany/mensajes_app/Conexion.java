7fabe152ec3bb07edbf8b4b8f5b219e4267815b7
DESKTOP-P5HC8GJ\MI EQUIPO <klisman.zapata@utp.edu.co>
2024-06-21 13:31:52 -0500

    My primer commit

---
+++ src/main/java/com/mycompany/mensajes_app/Conexion.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mensajes_app;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author MI EQUIPO
+ */
+public class Conexion {
+    
+    public Connection get_connection(){
+        Connection conection = null;
+        try{
+            conection = DriverManager.getConnection("jdbc:mysql://localhost:3306/mensajes_app", "root", "");
+            if(conection != null){
+                System.out.println("Conexión Esitosa!!!");
+            }
+        }catch(SQLException e){
+            System.out.println(e);
+        }
+        return conection;
+    }
+    
+}
